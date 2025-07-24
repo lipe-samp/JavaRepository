f02858ef38e02e0bf80a11b10b4ec3d1e364debe
Felipe Sampaio <felipe.sampaio198@gmail.com>
2025-07-24 18:59:43 -0300

    Commit for
    Incluindo exercícios de tabuada e a visualização de números ímpares ou pares entre dois números desejados pelo usuário. Além de somar todos eles.

--- Aula_0007 (estrutura for)/Aula_0007/src/Main.java
+++ Aula_0007 (estrutura for)/Aula_0007/src/Main.java
@@ -41,6 +41,80 @@ public class Main {
             System.out.println("Valor de i: " + i);
         }
 
+        // Mostrando os números ímpares ou pares dentro de dois valores inputados pelo usuário
+
+        int valor1, valor2, min, max, escolha;
+        int somandoImparesPares = 0;
+
+        System.out.println("Vamos visualizar todos os números ímpares ou pares entre dois números que você desejar.");
+        System.out.print("Digite [0] para ver os pares\nDigite [1] para ver os ímpares\n >> ");
+        escolha = sc.nextInt();
+
+        if (escolha == 0){
+            System.out.println("Você escolheu visualizar os números pares.");
+            System.out.print("Digite o primeiro número: ");
+            valor1 = sc.nextInt();
+            System.out.print("Digite o segundo número: ");
+            valor2 = sc.nextInt();
+
+            if (valor1 < valor2) {
+                min = valor1;
+                max = valor2;
+            } else {
+                min = valor2;
+                max = valor1;
+            }
+
+            System.out.println("Números pares de ["+min+"] até ["+max+"]");
+
+            for (int i = min; i <= max; i++) {
+                if (i % 2 == 0) {
+                    System.out.println(i);
+                    somandoImparesPares += i;
+                }
+            }
+
+            System.out.println("A soma dos números pares de ["+min+"] até ["+max+"] é igual a " + somandoImparesPares);
+
+        } else if (escolha == 1) {
+            System.out.println("Você escolheu visualizar os números ímpares.");
+            System.out.print("Digite o primeiro número: ");
+            valor1 = sc.nextInt();
+            System.out.print("Digite o segundo número: ");
+            valor2 = sc.nextInt();
+
+            if (valor1 < valor2) {
+                min = valor1;
+                max = valor2;
+            } else {
+                min = valor2;
+                max = valor1;
+            }
+
+            System.out.println("Números ímpares de ["+min+"] até ["+max+"]");
+
+            for (int i = min; i <= max; i++) {
+                if (i % 2 != 0) {
+                    System.out.println(i);
+                    somandoImparesPares += i;
+                }
+            }
+
+            System.out.println("A soma dos números pares de ["+min+"] até ["+max+"] é igual a " + somandoImparesPares);
+
+        } else {
+            System.out.println("Digitem somente [0] ou [1]");
+        }
+
+        // Realizando uma tabuada usando for
+
+        System.out.print("Deseja ver a tabuada no número \n >>");
+        int valorTabuada = sc.nextInt();
+
+        for (int i = 0; i <= 10; i++){
+            System.out.println(i + " x " + valorTabuada + " = " + i * valorTabuada );
+        }
+
         sc.close();
     }
 }
